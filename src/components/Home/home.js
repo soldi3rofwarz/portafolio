7050e0286a36446511b780d99e4b899afe4e5fe0
Soldi3rofwarz <josejehovanicastroperez@gmail.com>
2022-02-28 18:15:18 -0600

    agregando animaciones al header

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './styleHome.css'
 const Home = () => {
     return ( 
+        <React.Fragment>
         <section className='home' id='home'>
-            <div className='max-width'></div>
+            <div className='max-width'>
+                <div className='home-content'>
+                    <div className='text1'> Hola, mi nombre es</div>
+                    <div className='text2'> Jose Castro</div>
+                    <div className='text3'> Y soy Ing. en <span>Sistemas</span></div>
+                </div>
+            </div>
         </section>
-    );
+        <p>Dolor dolor duis pariatur incididunt ea reprehenderit Lorem cillum deserunt laborum elit velit veniam pariatur. Eu aliqua in cupidatat sit eu velit cillum nisi. Ad ad eiusmod voluptate non incididunt nulla quis et ad anim velit et. Fugiat mollit aute cupidatat adipisicing quis reprehenderit officia. Dolore veniam deserunt magna pariatur. Velit velit reprehenderit elit sint eiusmod irure consequat eu. Duis sint excepteur tempor ea proident aliqua veniam minim amet pariatur.</p>
+        <p>Dolor dolor duis pariatur incididunt ea reprehenderit Lorem cillum deserunt laborum elit velit veniam pariatur. Eu aliqua in cupidatat sit eu velit cillum nisi. Ad ad eiusmod voluptate non incididunt nulla quis et ad anim velit et. Fugiat mollit aute cupidatat adipisicing quis reprehenderit officia. Dolore veniam deserunt magna pariatur. Velit velit reprehenderit elit sint eiusmod irure consequat eu. Duis sint excepteur tempor ea proident aliqua veniam minim amet pariatur.</p>
+        <p>Dolor dolor duis pariatur incididunt ea reprehenderit Lorem cillum deserunt laborum elit velit veniam pariatur. Eu aliqua in cupidatat sit eu velit cillum nisi. Ad ad eiusmod voluptate non incididunt nulla quis et ad anim velit et. Fugiat mollit aute cupidatat adipisicing quis reprehenderit officia. Dolore veniam deserunt magna pariatur. Velit velit reprehenderit elit sint eiusmod irure consequat eu. Duis sint excepteur tempor ea proident aliqua veniam minim amet pariatur.</p>
+        <p>Dolor dolor duis pariatur incididunt ea reprehenderit Lorem cillum deserunt laborum elit velit veniam pariatur. Eu aliqua in cupidatat sit eu velit cillum nisi. Ad ad eiusmod voluptate non incididunt nulla quis et ad anim velit et. Fugiat mollit aute cupidatat adipisicing quis reprehenderit officia. Dolore veniam deserunt magna pariatur. Velit velit reprehenderit elit sint eiusmod irure consequat eu. Duis sint excepteur tempor ea proident aliqua veniam minim amet pariatur.</p>
+        </React.Fragment>
+
+    ); 
 }
  
 export default Home;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styleHeader.css'
+import menu from './../../assets/menu.png'
+
+
+
  const Header = () => {
+
+    const [scrollnav, setscrollnav]= useState(false)
+    const [toggle, settoggle]= useState(false)
+    
+  
+
+    const pressToggle=()=>{
+          let menu = document.querySelector(".toggle")
+    menu.onclick = function() {
+        menu.classList.toggle("action")
+    }
+
+    }
+    const scrolling=()=>{
+        if(window.scrollY >= 80){
+            setscrollnav(true)
+        }else{
+            setscrollnav(false)
+        }
+    }
+    window.addEventListener('scroll',scrolling)
      return (  
-         <nav className='navbar'>
+         <nav className={scrollnav? 'navbar active ': 'navbar'} >
              <div className='max-width'>
                 <div className='logo'><a href='#'>Portafo<span>lio.</span></a></div>
                 <ul className='menu'>
@@ -13,6 +38,11 @@ import './styleHeader.css'
                     <li><a href='#'>projects</a></li>
                     <li><a href='#'>contact</a></li>
                 </ul>
+                <div className='toggle' onClick={pressToggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
              </div>
          </nav>
      );
